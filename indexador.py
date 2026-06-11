2dd871f07240f9e486f165e7a8e300923ed822a2
Vicente-Alvarez <vicente.alvarez2@mail.udp.cl>
2026-06-10 20:31:12 -0400

    cambios

--- indexador.py
+++ indexador.py
@@ -1,13 +1,13 @@
 import os
 import pickle
 
-# Diccionario donde guardaremos los documentos
+
 indice = {}
 
-# Carpeta donde están los txt
+
 carpeta = "documentos"
 
-# Recorrer todos los archivos
+
 for archivo in os.listdir(carpeta):
 
     ruta = os.path.join(carpeta, archivo)
@@ -20,7 +20,7 @@ for archivo in os.listdir(carpeta):
 
         indice[archivo] = palabras
 
-# Guardar índice en disco
+
 with open("indice/indice.pkl", "wb") as f:
     pickle.dump(indice, f)
 
